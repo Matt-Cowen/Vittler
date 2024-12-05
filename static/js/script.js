223c1b0728cfce6c31286ac89e2fde9c69a3b4ee
Matthew Cowen <svlnsr3f@students.codeinstitute.net>
2024-12-05 14:07:25 +0000

    Initial styling: navbar + homepage

--- static/js/script.js
+++ static/js/script.js
@@ -14,22 +14,6 @@ const editButtons = document.getElementsByClassName("btn-edit");
 const titleText = document.getElementById("id_title");
 const deleteButtons = document.getElementsByClassName("btn-delete");
 
-/*
- * Initializes edit functionality for the provided edit buttons.
- * 
- * For each button in the `editButtons` collection:
- * - Retrieves the associated comment's ID upon click.
- * - Fetches the content of the corresponding comment.
- * - Populates the `commentText` input/textarea with the comment's content for editing.
- * - Updates the submit button's text to "Update".
- * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
- */
-
-// for (let button of submitButtons) {
-//     button.addEventListener("click", (e) => {
-//         recipeForm.setAttribute("action");
-//     });
-// }
 
 for (let button of editButtons) {
     button.addEventListener("click", (e) => {
@@ -49,12 +33,6 @@ for (let button of editButtons) {
     });
 };
 
-// for (let button of deleteButtons) {
-//     button.addEventListener("click", (e) => {
-//         let recipeId = e.target.getAttribute("data-recipe_id");
-//         deleteButtons.href = `library/recipe_delete/${recipeId}`
-//     })
-// }
 
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
